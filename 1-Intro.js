071d4dd334b5725a8a6b550c44dac03986bf0460
deepthi308 <deepthih6@gmail.com>
2023-03-22 10:14:32 +0530

    Node.js Concepts

---
+++ 1-Intro.js
@@ -0,0 +1,20 @@
+// Globals - No Window
+
+// __dirname - path to current directory
+// __filename - file name
+// __require - function to use modules
+// module - info about current module (file)
+// process - info about env where the program i being executed
+
+setInterval(() => {
+    console.log("Hello from the terminal");
+}, 2000)
+
+setTimeout(() => {
+ console.log("Hello from the terminal")   
+}, 0)
+
+console.log(__dirname);
+console.log(__filename)
+
+
---
+++ app.js
@@ -0,0 +1,10 @@
+const Deepthi_is_a_Multi_Millionaire = true;
+
+if (Deepthi_is_a_Multi_Millionaire == true) {
+    console.log("She is a queen")
+}
+else {
+    console.log("Trust me, She will become queen one day")
+}
+
+console.log("This is my first node app and I am so excited to start my journey from here 🤩");
